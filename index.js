e4ee416e700b9a3c6bfa87f1c8d9e005340b71e3
Matt Mowris <matt.mowris@gmail.com>
2017-03-09 20:33:45 -0800

    new

--- index.js
+++ index.js
@@ -1,123 +1,63 @@
-var express = require('express'),
-	app = express(),
-	account = require('./key.json');
+var gcm = require('node-gcm');
 
-app.set('port', (process.env.PORT || 5000));
+// Create a message
+// ... with default values
+var message = new gcm.Message();
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-var firebase = require("firebase");
-var request = require('request');
-
-// Initialize the app with a service account, granting admin privileges
-firebase.initializeApp({
-  databaseURL: process.env.DB_URL,
-  serviceAccount: account,
-});
-
-console.log("Firebase initialization passed");
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = firebase.database();
-var ref = db.ref("notifications");
-
-// This callback function is needed to detect changes in the number of attemps.
-// Note that a proper exponential backoff algorithm would work much better,
-// but just trying to send the notification 10 times is decent for testing / beta launch
-ref.orderByChild("sent").equalTo(false).on("child_changed", function(snapshot) {
-  console.log("Snapshot recieved (child_changed)");
-
-  handleNotificationSnapshot(snapshot)
-});
-
-// This callback function is needed to detect additions to notification/ in the Firebase databse
-ref.orderByChild("sent").equalTo(false).on("child_added", function(snapshot) {
-  console.log("Snapshot recieved (child_added)");
-
-  handleNotificationSnapshot(snapshot)
-});
-
-function handleNotificationSnapshot(snapshot) {
-  if (snapshot.val() != null) {   // Make sure that the snapshot is not empty
-
-    // Log the number of send attemps
-    console.log("Number of attemps " + snapshot.val()["attempts"])
-
-    // Double check that the notification has not been sent, and that the number of attemps is less than 10
-    if (snapshot.val()["sent"] === false && snapshot.val()["attempts"] < 10) {
-
-      console.log("VAL: ",snapshot.val());
-
-      // Create a reference to the notification ids (tokens) in the Firebase database
-      var userNotificationIDRef = db.ref("notification_ids/" + snapshot.val()["recipientUID"]);
-
-      // Retrieve the notification ids
-      userNotificationIDRef.once("value", function(notificationSnapshot) {
-
-        if (notificationSnapshot.val() != null) { // Make sure that the snapshot is not empty
-
-          console.log("Notification ids recieved");
-
-          // Send the notification
-          sendNotification(notificationSnapshot.val(), snapshot.val()["message"], snapshot.val()["key"], snapshot.val()["attempts"])
-        }
-        else {
-          console.log("No notification ids registered");
-        }
-      });
-
-    }
-  }
-}
-
-// This is a function which sends notifications to multiple devices
-function sendNotification(notificationID, message, key, attempts) {
-
-  console.log("Sending notification");
-
-  request(
-    {
-      method: 'POST',
-      uri: 'https://fcm.googleapis.com/fcm/send',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'key='+process.env.API_KEY
-      },
-      body: JSON.stringify({
-        "registration_ids": notificationID, // This is an aray of the users device tokens. Up to 1000 allowed by FCM
-        "priority": "high", // Change this value for different behavior on devices
-        "notification" : {
-          "body" : message,
-          "title": "Cleeq",
-          "sound": "default",
-          "badge": 0
-        }
-      })
+// ... or some given values
+var message = new gcm.Message({
+    collapseKey: 'demo',
+    priority: 'high',
+    contentAvailable: true,
+    delayWhileIdle: true,
+    timeToLive: 3,
+    restrictedPackageName: "somePackageName",
+    dryRun: true,
+    data: {
+        key1: 'message1',
+        key2: 'message2'
     },
-    function (error, response, body) {
-      if(response.statusCode == 200){
-
-        console.log('Success')
-
-        // Create a reference to the notification in the Firebase database
-        var notificationRef = db.ref("notifications/" + key);
-
-        // Set "sent" to true to avoid the notification being sent more than once
-        notificationRef.child("sent").set(true);
-
-        // Increment the "attempts"
-        notificationRef.child("attempts").set(attempts+1);
-      } else {
-        console.log('error: '+ response.statusCode)
-
-        // Create a reference to the notifications in the Firebase database
-        var notificationRef = db.ref("notifications/" + key);
-
-        // Increment the "attempts". Since "sent" is still false "child_changed" will be called
-        notificationRef.child("attempts").set(attempts+1);
-      }
+    notification: {
+        title: "Hello, World",
+        icon: "ic_launcher",
+        body: "This is a notification that will be displayed if your app is in the background."
     }
-  )
-}
+});
+
+// Change the message data
+// ... as key-value
+message.addData('key1','message1');
+message.addData('key2','message2');
+
+// ... or as a data object (overwrites previous data object)
+message.addData({
+    key1: 'message1',
+    key2: 'message2'
+});
+
+// Set up the sender with you API key
+var sender = new gcm.Sender(process.env.API_KEY);
+
+// Add the registration tokens of the devices you want to send to
+var registrationTokens = [];
+registrationTokens.push('dV_32YFU43Y:APA91bG0PqFtbxj_xbBz9-8dVZIP43TMAj31MixzoFrZO4WiyCqfAEEVA0CWVmyVKRLum497_Ko_OYG95JkyIGPOodZLMN7TJOCk7SWUY3B8fdGb0Amt6FbUJtPo_13CNSSEbLpIQqek');
+registrationTokens.push('881b965a8a4d2b0d0b6bc61fa4fd32b2289cfe7c');
+
+// Send the message
+// ... trying only once
+sender.sendNoRetry(message, { registrationTokens: registrationTokens }, function(err, response) {
+  if(err) console.error(err);
+  else    console.log(response);
+});
+
+// ... or retrying
+sender.send(message, { registrationTokens: registrationTokens }, function (err, response) {
+  if(err) console.error(err);
+  else    console.log(response);
+});
+
+// ... or retrying a specific number of times (10)
+sender.send(message, { registrationTokens: registrationTokens }, 10, function (err, response) {
+  if(err) console.error(err);
+  else    console.log(response);
+});
